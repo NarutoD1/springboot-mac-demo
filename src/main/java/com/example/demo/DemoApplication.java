fd972a80478bac77a082ebe2c12a8e7b42c6b0c2
miaocheng <15039190350@163.com>
2021-07-19 10:51:22 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        /*
+         * control + option + / 智能代码提示
+         * command + Y 小窗口显示鼠标悬停的类的内部信息
+         * command + U 快速打开实现的接口类
+         * command + B 定位到方法调用方
+         * command + D 复制当前行代码
+         * command + option + enter 上面另起一行
+         * command + shift + enter 下面另起一行
+         * command + option + L 格式化代码
+         * command + option + 方向键上/下 查看前一个或后一个打开的文件
+         * command + O 查找类
+         * control + shift + F 全局搜索文件内容
+         * option + control + O 导入包去掉不用的包
+         * option + shift + 方向键上/下 移动当前行到上一或下一行
+         */
+        System.out.println();
+    }
+
+}
